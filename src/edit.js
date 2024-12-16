434135a36324874b3b9c69c0c7275dee58841109
Dan Maby <danmaby@users.noreply.github.com>
2024-12-16 11:02:12 +0000

    feat: add src/edit.js and src/view.js files for YouTube Video Block plugin

---
+++ src/edit.js
@@ -0,0 +1,132 @@
+/**
+ * Retrieves the translation of text.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-i18n/
+ */
+import { __ } from "@wordpress/i18n";
+
+/**
+ * React hook that is used to mark the block wrapper element.
+ * It provides all the necessary props like the class name.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
+ */
+import {
+	useBlockProps,
+	MediaUpload,
+	InspectorControls,
+} from "@wordpress/block-editor";
+
+/**
+ * Import WordPress components PanelBody, TextControl, Button from the components library.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/components/
+ */
+import { PanelBody, TextControl, Button } from "@wordpress/components";
+
+/**
+ * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
+ * Those files can contain any CSS code that gets applied to the editor.
+ *
+ * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
+ */
+import "./editor.scss";
+
+/**
+ * The edit function describes the structure of your block in the context of the
+ * editor. This represents what the editor will render when the block is used.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#edit
+ *
+ * @return {Element} Element to render.
+ */
+export default function Edit({ attributes, setAttributes }) {
+	const { coverImage, videoURL } = attributes;
+
+	const blockProps = useBlockProps();
+
+	return (
+		<>
+			{/* Inspector Controls */}
+			<InspectorControls>
+				<PanelBody title={__("Block Settings", "yt-video-block")}>
+					<TextControl
+						label={__("Video URL", "yt-video-block")}
+						value={videoURL || ""} // Ensure a default value
+						onChange={(value) => setAttributes({ videoURL: value })}
+						placeholder={__("Enter a valid YouTube URL", "yt-video-block")}
+					/>
+					<MediaUpload
+						onSelect={(media) => setAttributes({ coverImage: media.url })}
+						allowedTypes={["image"]}
+						render={({ open }) => (
+							<Button onClick={open} isPrimary>
+								{__("Select Cover Image", "yt-video-block")}
+							</Button>
+						)}
+					/>
+				</PanelBody>
+			</InspectorControls>
+
+			{/* Preview Block */}
+			<div
+				{...blockProps}
+				style={{
+					position: "relative",
+					width: "100%",
+					maxWidth: "560px",
+					aspectRatio: "16/9",
+				}}
+			>
+				{coverImage && (
+					<img
+						src={coverImage}
+						alt={__("Video Cover", "yt-video-block")}
+						style={{
+							position: "absolute",
+							top: 0,
+							left: 0,
+							width: "100%",
+							height: "100%",
+							objectFit: "cover",
+						}}
+					/>
+				)}
+				<button
+					type="button"
+					style={{
+						position: "absolute",
+						top: "50%",
+						left: "50%",
+						transform: "translate(-50%, -50%)",
+						cursor: "pointer",
+						zIndex: 1,
+						textAlign: "center",
+						background: "rgba(0, 0, 0, 0.5)",
+						borderRadius: "50%",
+						width: "60px",
+						height: "60px",
+						display: "flex",
+						justifyContent: "center",
+						alignItems: "center",
+						border: "none",
+						padding: 0,
+						outline: "none",
+					}}
+				>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 24 24"
+						fill="white"
+						width="30px"
+						height="30px"
+						role="img"
+						aria-hidden="true"
+					>
+						<path d="M8 5v14l11-7z" />
+					</svg>
+				</button>
+			</div>
+		</>
+	);
+}
---
+++ src/view.js
@@ -0,0 +1,51 @@
+/**
+ * Use this file for JavaScript code that you want to run in the front-end
+ * on posts/pages that contain this block.
+ *
+ * When this file is defined as the value of the `viewScript` property
+ * in `block.json` it will be enqueued on the front end of the site.
+ *
+ * Example:
+ *
+ * ```js
+ * {
+ *   "viewScript": "file:./view.js"
+ * }
+ * ```
+ *
+ * If you're not making any changes to this file because your project doesn't need any
+ * JavaScript running in the front-end, then you should delete this file and remove
+ * the `viewScript` property from `block.json`.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-metadata/#view-script
+ */
+
+/* eslint-disable no-console */
+console.log("Hello World! (from create-block-yt-video-block block)");
+/* eslint-enable no-console */
+
+document.addEventListener("click", (event) => {
+	// Check if the clicked element is the play button inside the video block
+	const button = event.target.closest(
+		".wp-block-create-block-yt-video-block button"
+	);
+	if (!button) return;
+
+	// Get the parent container and the video URL from the data attribute
+	const container = button.closest(".wp-block-create-block-yt-video-block");
+	const videoURL = container.dataset.videoUrl;
+
+	if (videoURL) {
+		// Replace the container's content with the iframe
+		container.innerHTML = `
+            <iframe
+                width="100%"
+                height="100%"
+                src="${videoURL}?autoplay=1"
+                frameborder="0"
+                allowfullscreen
+                style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"
+            ></iframe>
+        `;
+	}
+});
